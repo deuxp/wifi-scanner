3bd91723eb8c6a4e6609888f2e8e410a18be7810
Gottfried Kleinberger <friedi.frieds@gmail.com>
2023-05-13 23:44:08 -0400

    home route setup

--- src/index.js
+++ src/index.js
@@ -2,28 +2,19 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const express = require("express");
 const morgan = require("morgan");
-const helmet = require("helmet");
 const cors = require("cors");
-// const bodyParser = require("body-parser");
-
-const { Pool } = require("pg");
-const { params } = require("./db/config");
-const db = new Pool(params);
-db.connect();
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json()); // access req.body
-// app.use(bodyParser.json()); // may not need
+app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-app.use(helmet());
 
 /* --------------------------------- ROUTES --------------------------------- */
-const messages = require("./routes/messages");
+const home = require("./routes/home");
 /* --------------------------------- ROUTES --------------------------------- */
-app.use("/api/messages", messages(db));
+app.use("/api/messages", home());
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
